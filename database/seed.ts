0245180c1223cf37b8c23ba8450121960e6ad7fa
maplemanju <kuzomegane@gmail.com>
2025-01-24 00:06:00 +0900

    update model

--- database/seed.ts
+++ database/seed.ts
@@ -19,6 +19,70 @@ async function main() {
       })
     }
   })
+
+  await prisma.roles.createMany({
+    data: [
+      {
+        id: 1,
+        roleName: 'admin',
+      },
+      {
+        id: 2,
+        roleName: 'moderator',
+        //TODO: add category
+      },
+      {
+        id: 3,
+        roleName: 'user',
+      },
+    ],
+  })
+
+  const mainCategory = await prisma.categories.create({
+    data: {
+      categoryName: 'General Discussion',
+      categoryDescription: 'A place for general forum discussions',
+      createdBy: 1,
+      updatedBy: 1,
+      childCategories: {
+        create: [
+          {
+            categoryName: 'Introductions',
+            categoryDescription: 'New members introduce yourself here',
+            createdBy: 1,
+            updatedBy: 1,
+          },
+          {
+            categoryName: 'Announcements',
+            categoryDescription: 'Important forum announcements',
+            createdBy: 1,
+            updatedBy: 1,
+          },
+        ],
+      },
+    },
+  })
+
+  // Seed posts
+  const welcomePost = await prisma.posts.create({
+    data: {
+      categoryId: mainCategory.id,
+      postTitle: 'Welcome to our Forum!',
+      postContent: 'Welcome to our new forum. We hope you enjoy your stay!',
+      createdBy: 1,
+      updatedBy: 1,
+    },
+  })
+
+  // Seed comments
+  await prisma.comments.create({
+    data: {
+      postId: welcomePost.id,
+      commentContent: 'Thanks for the warm welcome!',
+      createdBy: 1,
+      updatedBy: 1,
+    },
+  })
 }
 main()
   .then(async () => {
