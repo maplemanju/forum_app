b9b413e4031fbab10e7622a09eec42d19eec1dd9
maplemanju <kuzomegane@gmail.com>
2025-05-24 17:54:21 +0900

    add env

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL ?? '',
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '4MB',
